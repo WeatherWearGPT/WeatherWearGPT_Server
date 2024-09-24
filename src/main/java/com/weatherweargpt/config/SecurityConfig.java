81e5a176f7c848a532457128dfe705a5e37d366d
chuu8319 <chuu8319@gmail.com>
2024-09-24 13:52:17 +0900

    auth

--- src/main/java/com/weatherweargpt/config/SecurityConfig.java
+++ src/main/java/com/weatherweargpt/config/SecurityConfig.java
@@ -42,8 +42,8 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("*").permitAll());
-                       // .anyRequest().authenticated());
+                        .requestMatchers("*").permitAll()
+                        .anyRequest().authenticated());
 
         //세션 설정 : STATELESS
         http
